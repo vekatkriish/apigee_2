91f48dc5aa52d613516ea120fe630710141b65fa
venkatesh.k <venkat.kristipati@gmail.com>
2018-01-19 10:51:43 +0530

    added equals

--- RallyBuild/checkAndMerge.sh
+++ RallyBuild/checkAndMerge.sh
@@ -4,7 +4,12 @@ if [[ "$(git ls-remote --heads $1 $2 | wc -l)" == "1" ]]
 then
   git checkout $3
   git merge $2 -m "feature branch $2 merged"
-  echo "OK"
+  if [[ "$(git push https://$4:$5@$6 $7 --porcelain)" == *"Done"* ]]
+	then
+	  echo "OK"
+	else
+		echo "error"
+	fi
 else
   echo "error"
 fi
