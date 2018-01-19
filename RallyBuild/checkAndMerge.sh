7b5c37e8affd48f692182187d613de22491aaf45
venkatesh.k <venkat.kristipati@gmail.com>
2018-01-19 10:26:59 +0530

    param added before

--- RallyBuild/checkAndMerge.sh
+++ RallyBuild/checkAndMerge.sh
@@ -3,7 +3,7 @@
 if [[ "$(git ls-remote --heads $1 $2 | wc -l)" == "1" ]]
 then
   git checkout $3
-  git merge $2 -m "feature branch "'$2'" merged"
+  git merge $2 -m "feature branch $2 merged"
   echo "OK"
 else
   echo "error"
