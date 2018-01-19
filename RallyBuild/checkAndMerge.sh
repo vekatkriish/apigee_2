c47c5e220cff17f9a937262ec430957264f79c32
venkatesh.k <venkat.kristipati@gmail.com>
2018-01-19 10:18:57 +0530

    merege script added

--- RallyBuild/checkAndMerge.sh
+++ RallyBuild/checkAndMerge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$(git ls-remote --heads $1 $2 | wc -l)" == *"1"* ]]
+if [[ "$(git ls-remote --heads $1 $2 | wc -l)" == "1" ]]
 then
   git checkout $3
-  git merge $2 -m 'feature branch $2 merged'
+  git merge $2 -m "feature branch "'$2'" merged"
   echo "OK"
 else
   echo "error"
