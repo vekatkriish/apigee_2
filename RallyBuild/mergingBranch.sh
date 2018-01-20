9fae710997a9c175fb09f980ad32c874c48789dc
venkatesh.k <venkat.kristipati@gmail.com>
2018-01-21 00:17:48 +0530

    modified pipelinesystem jenkinsfileR

--- RallyBuild/mergingBranch.sh
+++ RallyBuild/mergingBranch.sh
@@ -5,7 +5,7 @@ if [[ "$(git ls-remote --heads $1 $2 | wc -l)" == *"1"* ]]
 then
   echo "checkout to $3...."
   git checkout $3
-  git merge $2 -m 'feature branch $1 merged'
+  git merge origin/$2 -m 'feature branch $1 merged'
   echo "branch $2 merged to $3.."
   
   echo "pushing the branch to remote repository...."
